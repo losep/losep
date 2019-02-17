2d7ed694e2ae1bbb7b392a7cae2d67c5a6a4f775
Eotect Nahn <eotect@gmail.com>
2019-02-17 17:01:05 +0800

    update totalcmd.bat

--- system/cmd/totalcmd.bat
+++ system/cmd/totalcmd.bat
@@ -1,8 +1,2 @@
 @echo off
-IF "AMD64" == "%PROCESSOR_ARCHITEW6432%" goto apprun64
-IF "AMD64" == "%PROCESSOR_ARCHITECTURE%" goto apprun64
-start "%FS_SYSTEM_ROOT%\app32\fileman\totalcmd\totalcmd.exe" %*
-goto end
-:apprun64
-apprund + "fileman\totalcmd\totalcmd64.exe" %*
-:end
+apprund64 fileman\totalcmd totalcmd64.exe totalcmd.exe
